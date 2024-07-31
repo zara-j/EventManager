d4691291a824b7c810121dfeb44521aca24c726e
javadi <javadi89s@gmail.com>
2024-07-31 14:44:53 +0330

    Update Project

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa';
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vitejs.dev/config/
@@ -10,11 +11,68 @@ export default defineConfig({
     vue({
       template: { transformAssetUrls }
     }),
-
-    // @quasar/plugin-vite options list:
-    // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
     quasar({
       sassVariables: 'src/quasar-variables.sass'
+    }),
+    VitePWA({
+      manifest: {
+        name: 'My App',
+        short_name: 'App',
+        description: 'My Progressive Web App',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#42b983',
+        icons: [
+          {
+            src: 'img/icons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'img/icons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      registerType: 'autoUpdate',
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.destination === 'document',
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'html-cache',
+            }
+          },
+          {
+            urlPattern: ({ request }) => request.destination === 'script',
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'js-cache',
+            }
+          },
+          {
+            urlPattern: ({ request }) => request.destination === 'style',
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'css-cache',
+            }
+          },
+          {
+            urlPattern: ({ request }) => request.destination === 'image',
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'image-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 30 // 30 days
+              }
+            }
+          }
+        ]
+      }
     })
   ]
 })
